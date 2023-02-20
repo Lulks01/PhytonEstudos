018b68820b7a928a962e3bbb7ce7133c25d677f7
Lulks <lucasfixina@gmail.com>
2023-02-20 15:51:54 -0300

    Update Soma com breakpoint.py

--- Soma com breakpoint.py
+++ Soma com breakpoint.py
@@ -1,2 +1,32 @@
-nums = 0
-trys = 0
+import random
+print('-==='*15)
+print('BEM-VINDO AO JOGO DE PAR OU IMPAR')
+wins = 0
+tipo = ' '
+print('-==='*15)
+while tipo not in 'PI':
+    tipo = str(input('Escolha entre Par ou Impar: [P/I] ')).strip().upper()[0]
+while True:
+    player = int(input('Digite um número de 0 à 10: '))
+    system = random.randint(1,11)
+    total = player + system
+    print(f'Você jogou {player} e o computador {system}, total de {total}')
+    if tipo == 'P':
+        if total % 2 == 0:
+            print('Você ganhou!')
+            wins +=1
+        else:
+            print('Você perdeu!')
+            break
+    elif tipo == 'I':
+        if total % 2 == 1:
+            print('Você ganhou!')
+            wins +=1
+        else:
+            print('Você perdeu!')
+            break
+    print('Vamos jogar novamente...')
+if wins == 0:
+    print('Você não ganhou nenhuma vez!')
+else:
+    print(f'Você ganhou {wins} vezes, mas infelizmente agora você perdeu!')
